6582f536e58100b8bda0b890fbbe0965e50254c4
Chizzy <chizoba.princess@gmail.com>
2018-12-06 14:36:52 -0500

    added seed data

--- tunr_react/settings.py
+++ tunr_react/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'rest_framework'
+    'rest_framework',
+    'main'
 ]
 
 MIDDLEWARE = [
